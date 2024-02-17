affba9ba0642578975f54128cdb0028865e5eadb
Tatin1 <Justine.boncalesss@gmail.com>
2024-02-18 00:00:04 +0800

    Lab Activity 2

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = 3000
 
 app.use(express.json())
 
-mongoose.connect('mongodb://127.0.0.1:27017/Justine',{
+mongoose.connect('mongodb://127.0.0.1/Justine',{
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
